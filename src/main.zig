7fc34bafbf8a638641ac441d53385ceeccdfcdaf
Ola Törnquist <ola.tornquist@gmail.com>
2026-01-04 14:23:04 +0000

    updated drawing

--- src/main.zig
+++ src/main.zig
@@ -4,8 +4,8 @@ const gol = @import("gol.zig");
 const rl = @import("raylib");
 
 pub fn main() !void {
-    const sizeX: u16 = 1280; //3840;
-    const sizeY: u16 = 720; //2160;
+    const sizeX: u16 = 3840;
+    const sizeY: u16 = 2160;
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
@@ -24,23 +24,41 @@ pub fn main() !void {
 
     while (!rl.windowShouldClose()) {
         rl.beginTextureMode(renderTexture);
-        rl.clearBackground(rl.Color.gray);
+        rl.clearBackground(rl.Color.dark_gray);
         for (0..sizeX) |x| {
             for (0..sizeY) |y| {
-                const index = x * @as(usize, @abs(world.sizeY)) + y;
+                const index = x * @abs(world.sizeY) + y;
                 if (world.map[index].alive == 1) {
-                    if (x > 1275 and y >= 719) std.Thread.sleep(1_000_000_000);
                     rl.drawPixel(@intCast(x), @intCast(y), rl.Color.green);
                 }
             }
         }
-        rl.drawFPS(5, 5);
+        const statusText = rl.textFormat("FPS: %i\nLife: %i", .{ rl.getFPS(), world.life });
+        rl.drawText(
+            statusText,
+            5,
+            5,
+            20,
+            rl.Color.black,
+        );
+        rl.drawText(
+            statusText,
+            4,
+            4,
+            20,
+            rl.Color.red,
+        );
         rl.endTextureMode();
 
         rl.beginDrawing();
         defer rl.endDrawing();
-        rl.drawTextureRec(renderTexture.texture, rl.Rectangle.init(0, 0, @as(f32, sizeX), -@as(f32, sizeY)), rl.Vector2.init(0, 0), rl.Color.white);
+        rl.drawTextureRec(
+            renderTexture.texture,
+            rl.Rectangle.init(0, 0, @as(f32, sizeX), -@as(f32, sizeY)),
+            rl.Vector2.init(0, 0),
+            rl.Color.white,
+        );
 
-        _ = world.update();
+        world.update();
     }
 }
